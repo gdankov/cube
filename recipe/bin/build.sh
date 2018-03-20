c9bd19c8886d5590d7babdf7069d728e870ec837
Julian Skupnjak <skupnjak@de.ibm.com>
2018-03-20 16:35:51 +0100

    add build script for docker image
    Signed-off-by: Matthias Diester <matthias.diester@de.ibm.com>

--- recipe/bin/build.sh
+++ recipe/bin/build.sh
@@ -1,4 +1,13 @@
+#!/bin/bash
+
 set -euo pipefail
-GOOS=linux go build -a -o recipe
-docker build --build-arg buildpacks="$(< "buildpacks.json")" -t "dvrs/recipe:build" .
-docker run -it -e 'APP_ID=19791b75-7cad-4b34-83f8-db636427d673' -e 'STAGING_GUID=staging-guid' --rm -v "$(pwd)/out:/out" dvrs/recipe:build
+
+BASEDIR="$(cd $(dirname $0)/.. && pwd)"
+
+( cd $BASEDIR && GOOS=linux go build -a -o $BASEDIR/image/recipe )
+( cd $BASEDIR/packs/cf/builder && GOOS=linux CGO_ENABLED=0 go build -a -installsuffix static -o $BASEDIR/image/builder )
+
+pushd $BASEDIR/image
+docker build --build-arg buildpacks="$(< "buildpacks.json")" -t "cube/recipe:build" .
+popd
+
